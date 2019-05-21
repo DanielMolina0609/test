ac9bce9fc16cf4473e394b4d48baadf40aa3e2db
DanielMolina0609 <molinacabreradaniel@gmail.com>
2019-05-21 13:20:35 -0700

    Implementacion de bootstrap al formulario de login

---
+++ app/Http/Controllers/VueJSController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+
+class VueJSController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('login');
+    }
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'pass' => 'required'
+        ]);
+
+
+        return response()->json(['success'=>'Done!']);
+    }
+}
